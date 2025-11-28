e79d5642459c0a2006ba13c7d3d9e1befb62864d
afridaasad <afreedaasad.1808@gmail.com>
2025-11-28 20:07:13 +0530

    added leftover sections

--- src/pages/Employer/EmployerLanding.jsx
+++ src/pages/Employer/EmployerLanding.jsx
@@ -5,6 +5,7 @@ import TopCompanies from "../../components/Employer/TopCompanies";
 import PostJob from "../../components/Employer/PostJob";
 import DatabaseSection from "../../components/Employer/DatabaseSection";
 import TestimonialsSection from "../../components/Employer/TestimonialsSection";
+import BlogSection from "../../components/Employer/BlogSection";
 import Footer from "../../components/common/Footer/FooterMain";
 
 export default function EmployerLanding() {
@@ -17,6 +18,7 @@ export default function EmployerLanding() {
       <PostJob />
       <DatabaseSection />
       <TestimonialsSection />
+      <BlogSection />
       <Footer />
     </>
   );
